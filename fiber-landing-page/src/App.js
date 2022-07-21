352ab35b5777b4c425f6b283709d0df631b8f390
Balraj Johal <balrajsjohal@gmail.com>
2022-07-21 21:59:27 +0100

    add some animatiion to box

--- fiber-landing-page/src/App.js
+++ fiber-landing-page/src/App.js
@@ -1,4 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+
+import * as THREE from "three";
 import { Canvas, useFrame } from "@react-three/fiber";
 
 import "./App.css";
@@ -11,17 +13,35 @@ export default function App() {
     );
 }
 
+
+const INIT_HUE = 200;
+const END_HUE = 250;
+
 function Box(props) {
     const box = useRef();
+    const [hue, setHue] = useState(100);
+    const [clicked, setClicked] = useState(false);
 
-    useFrame(() => {
-        box.current.rotation.z += 0.01;
+    useFrame((state) => {
+        const curr = box.current;
+        curr.position.z = THREE.MathUtils.lerp(
+            curr.position.z, clicked ? 1 : 0, 0.125
+        )
+        const rotator = Math.sin(state.clock.elapsedTime) * Math.PI;
+        curr.rotation.x = rotator;
+        curr.rotation.z = state.clock.elapsedTime;
+
+        const newHue = THREE.MathUtils.lerp(
+            hue, clicked ? END_HUE : INIT_HUE, 0.125
+        )
+        setHue(newHue);
+        console.log(hue);
     })
 
     return(
-        <mesh position={[0,0,0]} ref={box} >
+        <mesh position={[0,0,0]} ref={box} onClick={() => { setClicked(!clicked) }}>
             <boxGeometry />
-            <meshBasicMaterial color={"hotpink"} />
+            <meshBasicMaterial color={`hsl(${hue}, 100%, 50%)`} />
         </mesh>
     )
 }
